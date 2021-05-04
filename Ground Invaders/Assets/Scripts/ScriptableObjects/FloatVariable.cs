461d4d51906ce6ebfdb25f03fa2d7d9de87c689e
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-04 18:44:01 +0200

    commit FloatVariable scriptable object

---
+++ Ground Invaders/Assets/Scripts/ScriptableObjects/FloatVariable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatVariable : ScriptableObject
+{
+    #region fields
+    public float Value;
+    #endregion fields
+}
