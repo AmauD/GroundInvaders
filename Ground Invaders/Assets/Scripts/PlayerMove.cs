6cde92fa9b5a0a6ad955421f16e7a03f2b5b8bc8
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-04 16:57:08 +0200

    commit player movement

--- Ground Invaders/Assets/Scripts/PlayerMove.cs
+++ Ground Invaders/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region properties
+
+    #endregion properties
+
+
+
+    #region fields
+    private Rigidbody _rigidbody;
+    private Vector3 _movementInput;
+    [SerializeField] [Range(0f, 50f)] private float _speed = 2f;
+    #endregion fields
+
+
+
+    #region publics methods
+
+    #endregion publics methods
+
+
+
+    #region unity messages
+    private void Awake()
     {
-        
+        if (_rigidbody == null)
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateInputMove();
     }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    #endregion unity messages
+
+
+
+    #region privates methods
+    private void UpdateInputMove()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+
+        _movementInput = new Vector3(horizontal, 0f, 0f);
+    }
+
+    private void Move()
+    {
+        var Velocity = _movementInput * _speed;
+
+        _rigidbody.velocity = Velocity;
+    }
+    #endregion privates methods
 }
