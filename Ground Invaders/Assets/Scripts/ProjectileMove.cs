f3a797fe5b6dba32aeac2fd0c427bcccc1a1aaaa
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-05 09:21:06 +0200

    commit add EnemyProjectile prefab + material + script

---
+++ Ground Invaders/Assets/Scripts/ProjectileMove.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    #region properties
+
+    #endregion properties
+
+
+
+    #region fields
+    private Transform _transform;
+    private Rigidbody _rigidbody;
+    private float _speed = 0.1f;
+    #endregion fields
+
+
+
+    #region public methods
+
+    #endregion public methods
+
+
+
+    #region unity messages
+    private void Awake()
+    {
+        if (_transform == null) {_transform = GetComponent<Transform>();}
+        if (_rigidbody == null) {_rigidbody = GetComponent<Rigidbody>();}
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    #endregion unity messages
+
+
+
+    #region private methods
+    private void Move()
+    {
+        var velocity = _transform.forward * _speed;
+        var newPosition = _transform.position + velocity;
+
+        _rigidbody.MovePosition(newPosition);
+    }
+    #endregion private methods
+}
