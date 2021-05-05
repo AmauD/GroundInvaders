df7b51f0213abc0a30aa7a8d1c93f25d030184f0
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-05-05 08:55:22 +0200

    Add Cannon Angle

---
+++ Ground Invaders/Assets/Scripts/CannonAngle.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CannonAngle : MonoBehaviour
+{
+    #region fields
+    private Transform _transform;
+    private float _speed = 5f;
+    #endregion
+
+
+
+    #region publics methods
+
+    private void OrientationCannon()
+    {
+        var angle = _transform.eulerAngles;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            angle.z -= _speed;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            angle.z += _speed;
+        }
+
+        angle.z = Mathf.Clamp(angle.z, 310f, 359.9f);
+
+        _transform.eulerAngles = angle;
+    }
+
+    #endregion
+
+
+
+    #region unity messages
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        OrientationCannon();
+    }
+
+    #endregion
+
+}
