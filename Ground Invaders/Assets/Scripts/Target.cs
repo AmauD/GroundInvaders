631b249434213757e8581523a1392eded594679a
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-06 20:16:53 +0200

    add target prefab + script

--- Ground Invaders/Assets/Scripts/Target.cs
+++ Ground Invaders/Assets/Scripts/Target.cs
@@ -11,11 +11,15 @@ public class Target : MonoBehaviour
 
 
     #region fields
-    [SerializeField] private CannonAngle _cannonRotation;
+    private CannonAngle _cannonRotation;
     private Transform _transform;
-    private float _minZPosition;
-    private float _maxZPosition;
     #endregion fields
+
+
+
+    #region public methods
+
+    #endregion public methods
 
 
 
@@ -24,16 +28,16 @@ public class Target : MonoBehaviour
     {
         if (_transform == null) { _transform = GetComponent<Transform>(); }
         if (_cannonRotation == null) {_cannonRotation = GetComponent<CannonAngle>();}
-        _minZPosition = _transform.position.z;
-        _maxZPosition = 7.5f;
+    }
+
+    private void Start()
+    {
+        
     }
 
     private void Update()
     {
-        var _closePosition = new Vector3(_transform.position.x, 0.015f, _minZPosition);
-        var _farPosition = new Vector3(_transform.position.x, 0.015f, _maxZPosition);
-
-        _transform.position = Vector3.Lerp(_closePosition, _farPosition, _cannonRotation.GetCannonRotation());
+        
     }
     #endregion unity messages
 
