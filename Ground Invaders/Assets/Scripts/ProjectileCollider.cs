41e2dd466d67866bc679dccdc1286d5d1175989a
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-05-06 17:15:42 +0200

    Add bool CanShoot for delay

--- Ground Invaders/Assets/Scripts/ProjectileCollider.cs
+++ Ground Invaders/Assets/Scripts/ProjectileCollider.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class ProjectileCollider : MonoBehaviour
 {
+    #region fields
+    private CannonShoot _cannon;
+
+    #endregion
+
+    #region publics methods
+
+    public void Init(CannonShoot cannon)
+    {
+        _cannon = cannon;
+    }
+
+    #endregion
+
+
+
     #region unity messages
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Destroy");
         Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        _cannon.CanShoot = true;
     }
     #endregion unity messages
 }
