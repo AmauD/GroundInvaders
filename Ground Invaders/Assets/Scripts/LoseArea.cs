fddc62944a3c7cb1dcd9c15ca518e8319471fa9f
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-07 14:43:32 +0200

    add lose area + enemy corpse fade over time

--- Ground Invaders/Assets/Scripts/LoseArea.cs
+++ Ground Invaders/Assets/Scripts/LoseArea.cs
@@ -6,12 +6,15 @@ public class LoseArea : MonoBehaviour
 {
     #region fields
     [SerializeField] LevelManager _levelManager;
+    [SerializeField] BoolVariable _playerIsAlive;
     #endregion fields
 
 
     #region unity messages
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Enemy enter");
+        _playerIsAlive.Value = false;
         _levelManager.Lose();
     }
     #endregion unity messages
