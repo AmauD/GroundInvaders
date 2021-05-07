3923e376a74680d5afb2a77b24252ce6a388b89c
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-07 15:49:30 +0200

    add tank destroyed prefab

--- Ground Invaders/Assets/Scripts/PlayerCollider.cs
+++ Ground Invaders/Assets/Scripts/PlayerCollider.cs
@@ -6,15 +6,23 @@ public class PlayerCollider : MonoBehaviour
 {
 
     #region fields
+    private Transform _transform;
     [SerializeField] AudioClip _audioClip;
     [SerializeField] AudioManager _audioManager;
     [SerializeField] BoolVariable _playerIsAlive;
+    [SerializeField] GameObject _tankDestroyedPrefab;
 
     #endregion
     #region unity messages
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         _playerIsAlive.Value = false;
+        Instantiate(_tankDestroyedPrefab, _transform.position, _transform.rotation);
         Destroy(gameObject);
         _audioManager.Play(_audioClip);
     }
