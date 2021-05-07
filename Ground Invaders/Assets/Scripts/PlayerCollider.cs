e829a6ce7cb507135bfd015915f8ca58e66741d5
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-07 12:14:49 +0200

    add scene restart

--- Ground Invaders/Assets/Scripts/PlayerCollider.cs
+++ Ground Invaders/Assets/Scripts/PlayerCollider.cs
@@ -8,10 +8,13 @@ public class PlayerCollider : MonoBehaviour
     #region fields
     [SerializeField] AudioClip _audioClip;
     [SerializeField] AudioManager _audioManager;
+    [SerializeField] BoolVariable _playerIsAlive;
+
     #endregion
     #region unity messages
     private void OnCollisionEnter(Collision collision)
     {
+        _playerIsAlive.Value = false;
         Destroy(gameObject);
         _audioManager.Play(_audioClip);
     }
