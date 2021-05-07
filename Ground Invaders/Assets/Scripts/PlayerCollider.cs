bce166af36f6700bc10a029f0267ffc1e049bb13
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-05-07 11:19:49 +0200

    Add AudioManager + Sound when the player died

--- Ground Invaders/Assets/Scripts/PlayerCollider.cs
+++ Ground Invaders/Assets/Scripts/PlayerCollider.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerCollider : MonoBehaviour
 {
+
+    #region fields
+    [SerializeField] AudioClip _audioClip;
+    [SerializeField] AudioManager _audioManager;
+    #endregion
     #region unity messages
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
+        _audioManager.Play(_audioClip);
     }
     #endregion unity messages
 }
--- Ground Invaders/Assets/Scripts/ProjectileCollider.cs
+++ Ground Invaders/Assets/Scripts/ProjectileCollider.cs
@@ -31,5 +31,6 @@ public class ProjectileCollider : MonoBehaviour
     {
         _cannon.CanShoot = true;
     }
+
     #endregion unity messages
 }
